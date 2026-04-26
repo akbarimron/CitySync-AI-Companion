6bc961b416dea4ac8c3ee11395be85fe1c7a6b9f
gracelaulina <144015658+gracelaulina@users.noreply.github.com>
2026-04-26 11:42:17 +0700

    SET UP DATABASE SCHEMA USERS CATEGORIES DESTINATIONS USER_PREFERENCES AND CHAT SYSTEM

---
+++ database/migrations/2026_04_26_043518_create_chat_history_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_history', function (Blueprint $table) {
+            $table->id(); // message_id
+
+            $table->foreignId('session_id')
+                ->constrained('chat_sessions')
+                ->cascadeOnDelete();
+
+            $table->enum('sender', ['User', 'AI']);
+            $table->text('message_text');
+            $table->timestamp('sent_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_history');
+    }
+};
