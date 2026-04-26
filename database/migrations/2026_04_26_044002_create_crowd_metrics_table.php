8a8320b64e69ad3a19c4927a81e5dfdb2f203ed1
Ryan Yanuar Pradana <ryanyanuarpradana@gmail.com>
2026-04-26 14:50:19 +0700

    set up table itinenaries

--- database/migrations/2026_04_26_044002_create_crowd_metrics_table.php
+++ database/migrations/2026_04_26_044002_create_crowd_metrics_table.php
@@ -10,21 +10,22 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('crowd_metrics', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('device_id')
-                ->constrained()
+{
+    Schema::create('crowd_metrics', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('device_id')
+                ->constrained('iot_devices')
                 ->cascadeOnDelete();
 
-            $table->dateTime('timestamp');
-            $table->integer('occupancy_count');
+        $table->dateTime('timestamp');
+        $table->integer('occupancy_count');
 
-            $table->foreignId('weather_id')
-                ->constrained()
+        $table->foreignId('weather_id')
+                ->constrained('weather_reference')
                 ->cascadeOnDelete();
-        });
-    }
+    });
+}
 
     /**
      * Reverse the migrations.
